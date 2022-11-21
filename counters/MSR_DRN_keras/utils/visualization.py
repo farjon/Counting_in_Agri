550e2aa420d04878a53aebb2440c6ffa940c6ecc
farjon <guyfar@post.bgu.ac.il>
2022-11-21 10:14:43 +0200

    resuming loading working version

--- counters/MSR_DRN_keras/utils/visualization.py
+++ counters/MSR_DRN_keras/utils/visualization.py
@@ -1,24 +1,10 @@
-"""
-Copyright 2017-2018 Fizyr (https://fizyr.com)
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
-
+import os
 import cv2
+import warnings
 import numpy as np
-
-from .colors import label_color
-
+from PIL import Image
+import matplotlib.pyplot as plt
+from .read_activations import get_activations
 
 def draw_box(image, box, color, thickness=2):
     """ Draws a box on an image with a given color.
@@ -98,3 +84,227 @@ def draw_annotations(image, annotations, color=(0, 255, 0), label_to_name=None):
         draw_caption(image, a, caption)
 
         draw_box(image, a, color=c)
+
+
+
+def show_annotations_on_image(image, annotations, save_path):
+    import PIL.ImageDraw as ImageDraw
+    import PIL.Image as Image
+    import copy
+    im_drawing = Image.fromarray(np.uint8(copy.deepcopy(image)))
+    draw_mask = ImageDraw.Draw(im_drawing)
+    for point in annotations:
+        x_tl = round(int(point[0]) - 10)
+        x_br = round(int(point[0]) + 10)
+        y_tl = round(int(point[1]) - 10)
+        y_br = round(int(point[1]) + 10)
+        draw_mask.ellipse([x_tl, y_tl, x_br, y_br], fill='blue')
+    im_drawing.save(save_path)
+
+
+def plot_RP_curve(recall, precision, ap, save_path):
+    from matplotlib import rcParams
+    rcParams.update({'figure.autolayout': True})
+    plt.figure()
+    plt.rcParams.update({'font.size': 20})
+    plt.step(recall, precision, color='b', alpha=0.99)
+    plt.fill_between(recall, precision, step='post', color='b', alpha=0.1, linewidth=3)
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title('AP={0:0.2f}'.format(ap))
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    plot_path = os.path.join(save_path + '\\RP_curve.png')
+    plt.savefig(plot_path)
+    plt.close(plot_path)
+
+def visualize_images(output, Image_name, save_path, generator, model, image):
+    if not generator.epoch == None:
+        current_epoch = str(generator.epoch+1) #generator.epoch in [0,99]
+    else:
+        current_epoch = 'test'
+
+    visualization_path = os.path.join(save_path, 'epoch_' + current_epoch)
+
+    if not os.path.exists(visualization_path):
+        os.makedirs(visualization_path)
+
+    fgImage_name = Image_name + "_fg.png"
+
+    # Draw GT activations:
+    #plt.figure()
+    #plt.axis("off")
+    background = Image.open(os.path.join(generator.base_dir, fgImage_name), 'r')
+    background = background.convert("RGBA")
+    BG_w, BG_h = background.size
+    # plt.imshow(background)
+    # plt.savefig(visualization_path + '/' + Image_name + '_BG.png', pad_inches=0)  # transparent=True,
+    # plt.close()
+
+    anno = output[2][0, :, :, 0]
+    plt.figure()
+    #heat_map = seaborn.heatmap(anno, xticklabels=False, yticklabels=False, cbar=False)
+    #heat_map = heat_map.despine
+    #heat_map = heat_map.get_figure()
+    plt.imshow(anno)
+    plt.imsave(visualization_path + '/' + Image_name + '_anno.png', anno)
+    gt_anns = Image.open(visualization_path + '/' + Image_name + '_anno.png')
+    gt_anns = gt_anns.resize((BG_w, BG_h))  # Image.ANTIALIAS
+    plt.imsave(visualization_path + '/' + Image_name + '_anno.png', gt_anns)
+    plt.close()
+
+    # out = image1 * (1.0 - alpha) + image2 * alpha
+    plt.figure()
+    plt.axis("off")
+    alphaBlended = Image.blend(gt_anns, background, 0.6)
+    plt.imshow(alphaBlended)
+    plt.imsave(visualization_path + '/' + Image_name + '_Blended_GT.png',alphaBlended )
+    plt.close()
+
+    # Relu map #######################################################################################################
+    plt.figure()
+    classification_submodel_activations = get_activations(model, model_inputs=image[0], print_shape_only=False,
+                                                          layer_name='pyramid_classification_relu')
+    classification_submodel_activations = classification_submodel_activations[0][0, :, :, 0]
+
+    plt.imshow(classification_submodel_activations)
+    plt.imsave(visualization_path + '/' + Image_name + '_Relu.png', classification_submodel_activations)
+    relu_anns = Image.open(visualization_path + '/' + Image_name + '_Relu.png')
+
+    #relu_anns = relu_anns.convert("RGBA")
+    relu_anns = relu_anns.resize((BG_w, BG_h))  # Image.ANTIALIAS
+    plt.imsave(visualization_path + '/' + Image_name + '_Relu.png', relu_anns)
+    plt.close()
+
+    plt.figure()
+    plt.axis("off")
+    alphaBlended_relu = Image.blend(relu_anns, background, 0.6)
+    plt.imshow(alphaBlended_relu)
+    plt.imsave(visualization_path + '/' + Image_name + '_Blended_Relu.png', alphaBlended_relu)
+    plt.close()
+
+    # softmax map #####################################################################################################
+
+    plt.figure()
+    local_soft_max_activations = get_activations(model, model_inputs=image[0], print_shape_only=False,
+                                                 layer_name='LocalSoftMax')
+    local_soft_max_activations = local_soft_max_activations[0][0, :, :, 0]
+
+    plt.imshow(local_soft_max_activations)
+    plt.imsave(visualization_path + '/' + Image_name + '_softmax.png', local_soft_max_activations)
+    softmax_anns = Image.open(visualization_path + '/' + Image_name + '_softmax.png')
+
+    #softmax_anns = softmax_anns.convert("RGBA")
+    softmax_anns = softmax_anns.resize((BG_w, BG_h))  # Image.
+    plt.imsave(visualization_path + '/' + Image_name + '_softmax.png', softmax_anns)
+    plt.close()
+
+    plt.figure()
+    plt.axis("off")
+    alphaBlended_softmax = Image.blend(softmax_anns, background, 0.6)
+    plt.imshow(alphaBlended_softmax)
+    plt.imsave(visualization_path + '/' + Image_name + '_Blended_softmax.png', alphaBlended_softmax)
+    plt.close()
+
+def label_color(label):
+    """ Return a color from a set of predefined colors. Contains 80 colors in total.
+
+    Args
+        label: The label to get the color for.
+
+    Returns
+        A list of three values representing a RGB color.
+
+        If no color is defined for a certain label, the color green is returned and a warning is printed.
+    """
+    if label < len(colors):
+        return colors[label]
+    else:
+        warnings.warn('Label {} has no color, returning default.'.format(label))
+        return (0, 255, 0)
+
+
+colors = [
+    [31  , 0   , 255] ,
+    [0   , 159 , 255] ,
+    [255 , 95  , 0]   ,
+    [255 , 19  , 0]   ,
+    [255 , 0   , 0]   ,
+    [255 , 38  , 0]   ,
+    [0   , 255 , 25]  ,
+    [255 , 0   , 133] ,
+    [255 , 172 , 0]   ,
+    [108 , 0   , 255] ,
+    [0   , 82  , 255] ,
+    [0   , 255 , 6]   ,
+    [255 , 0   , 152] ,
+    [223 , 0   , 255] ,
+    [12  , 0   , 255] ,
+    [0   , 255 , 178] ,
+    [108 , 255 , 0]   ,
+    [184 , 0   , 255] ,
+    [255 , 0   , 76]  ,
+    [146 , 255 , 0]   ,
+    [51  , 0   , 255] ,
+    [0   , 197 , 255] ,
+    [255 , 248 , 0]   ,
+    [255 , 0   , 19]  ,
+    [255 , 0   , 38]  ,
+    [89  , 255 , 0]   ,
+    [127 , 255 , 0]   ,
+    [255 , 153 , 0]   ,
+    [0   , 255 , 255] ,
+    [0   , 255 , 216] ,
+    [0   , 255 , 121] ,
+    [255 , 0   , 248] ,
+    [70  , 0   , 255] ,
+    [0   , 255 , 159] ,
+    [0   , 216 , 255] ,
+    [0   , 6   , 255] ,
+    [0   , 63  , 255] ,
+    [31  , 255 , 0]   ,
+    [255 , 57  , 0]   ,
+    [255 , 0   , 210] ,
+    [0   , 255 , 102] ,
+    [242 , 255 , 0]   ,
+    [255 , 191 , 0]   ,
+    [0   , 255 , 63]  ,
+    [255 , 0   , 95]  ,
+    [146 , 0   , 255] ,
+    [184 , 255 , 0]   ,
+    [255 , 114 , 0]   ,
+    [0   , 255 , 235] ,
+    [255 , 229 , 0]   ,
+    [0   , 178 , 255] ,
+    [255 , 0   , 114] ,
+    [255 , 0   , 57]  ,
+    [0   , 140 , 255] ,
+    [0   , 121 , 255] ,
+    [12  , 255 , 0]   ,
+    [255 , 210 , 0]   ,
+    [0   , 255 , 44]  ,
+    [165 , 255 , 0]   ,
+    [0   , 25  , 255] ,
+    [0   , 255 , 140] ,
+    [0   , 101 , 255] ,
+    [0   , 255 , 82]  ,
+    [223 , 255 , 0]   ,
+    [242 , 0   , 255] ,
+    [89  , 0   , 255] ,
+    [165 , 0   , 255] ,
+    [70  , 255 , 0]   ,
+    [255 , 0   , 172] ,
+    [255 , 76  , 0]   ,
+    [203 , 255 , 0]   ,
+    [204 , 0   , 255] ,
+    [255 , 0   , 229] ,
+    [255 , 133 , 0]   ,
+    [127 , 0   , 255] ,
+    [0   , 235 , 255] ,
+    [0   , 255 , 197] ,
+    [255 , 0   , 191] ,
+    [0   , 44  , 255] ,
+    [50  , 255 , 0]
+]
